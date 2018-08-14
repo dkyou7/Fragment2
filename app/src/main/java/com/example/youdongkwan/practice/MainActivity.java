c4b7b48439f8094879c87a7aefa1b6a2818e70bf
youdongkwan <dkyou7@naver.com>
2018-08-14 22:12:06 +0900

    Initial commit

---
+++ app/src/main/java/com/example/youdongkwan/practice/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.youdongkwan.practice;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListFragment listFragment;
+    ViewerFragment viewerFragment;
+
+    FragmentManager manager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        manager = getSupportFragmentManager();
+
+        listFragment = (ListFragment) manager.findFragmentById(R.id.listFragment);
+        viewerFragment = (ViewerFragment) manager.findFragmentById(R.id.viewerFragment);
+    }
+
+    public void onImageChange(int index){
+        viewerFragment.setImage(index);
+    }
+}
---
+++ app/src/main/java/com/example/youdongkwan/practice/ViewerFragment.java
@@ -0,0 +1,35 @@
+package com.example.youdongkwan.practice;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+public class ViewerFragment extends Fragment {
+
+    ImageView imageView;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_viewer,container,false);
+
+        imageView = (ImageView)rootView.findViewById(R.id.imageView);
+
+        return rootView;
+    }
+
+    public void setImage(int index){
+        if(index ==0){
+            imageView.setImageResource(R.drawable.image1);
+        }else if(index ==1){
+            imageView.setImageResource(R.drawable.image2);
+        }else if(index ==2){
+            imageView.setImageResource(R.drawable.image3);
+        }
+    }
+}
